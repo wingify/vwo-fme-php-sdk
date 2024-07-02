577843a80d2df8124ea1a37d2efa5a14acaaa3f6
sakshamg1304 <sakshamg1304@gmail.com>
2024-07-02 17:45:22 +0530

    test: add E2E tests to ensure everything is working

---
+++ tests/e2e/TrackEventTest.php
@@ -0,0 +1,128 @@
+<?php
+
+/**
+ * Copyright 2024 Wingify Software Pvt. Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace vwo;
+
+use PHPUnit\Framework\TestCase;
+use vwo\VWOBuilder;
+use vwo\VWO;
+use vwo\SettingsAndTestCases;
+
+class TrackEventTest extends TestCase
+{
+    protected $options;
+
+    protected function setUp(): void
+    {
+        $vwoBuilder = new VWOBuilder([
+            'accountId' => '123456',
+            'sdkKey' => 'abcdef'
+        ]);
+        $vwoBuilder->setLogger();
+        $vwoBuilder->setSettings(SettingsAndTestCases::get()['BASIC_ROLLOUT_SETTINGS']);
+
+        $this->options = [
+            'sdkKey' => 'sdk-key',
+            'accountId' => 'account-id',
+            'vwoBuilder' => $vwoBuilder, // pass only for E2E tests
+        ];
+    }
+
+
+    public function testTrackEventSuccessfully()
+    {
+        $vwoClient = VWO::init($this->options);
+
+        // Mock input data
+        $eventName = 'custom1';
+        $eventProperties = ['key' => 'value'];
+        $context = ['id' => '123'];
+
+        // Call the trackEvent method
+        $result = $vwoClient->trackEvent($eventName, $context, $eventProperties);
+
+        // Assert that the method resolves with the correct data
+        $this->assertEquals([$eventName => true], $result);
+    }
+
+    public function testTrackEventWithoutMetric()
+    {
+        $vwoClient = VWO::init($this->options);
+
+        // Mock input data
+        $eventName = 'testEvent';
+        $eventProperties = ['key' => 'value'];
+        $context = ['id' => '123'];
+
+        // Call the trackEvent method
+        $result = $vwoClient->trackEvent($eventName, $context, $eventProperties);
+
+        // Assert that the method resolves with the correct data
+        $this->assertEquals([$eventName => false], $result);
+    }
+
+    public function testTrackEventWithInvalidEventName()
+    {
+        $vwoClient = VWO::init($this->options);
+
+        // Mock input data with invalid eventName
+        $eventName = 123; // Invalid eventName
+        $eventProperties = ['key' => 'value'];
+        $context = ['id' => '123'];
+
+        // Call the trackEvent method
+        $result = $vwoClient->trackEvent($eventName, $context, $eventProperties);
+
+        // Assert that the method resolves with the correct data
+        $this->assertEquals([$eventName => false], $result);
+    }
+
+    public function testTrackEventWithInvalidEventProperties()
+    {
+        $vwoClient = VWO::init($this->options);
+
+        // Mock input data with invalid eventProperties
+        $eventName = 'testEvent';
+        $eventProperties = 'invalid'; // Invalid eventProperties
+        $context = ['id' => '123'];
+
+        // Call the trackEvent method
+        $result = $vwoClient->trackEvent($eventName, $context, $eventProperties);
+
+        // Assert that the method resolves with the correct data
+        $this->assertEquals([$eventName => false], $result);
+    }
+
+    public function testTrackEventWithInvalidContext()
+    {
+        $vwoClient = VWO::init($this->options);
+
+        // Mock input data with invalid context
+        $eventName = 'testEvent';
+        $eventProperties = ['key' => 'value'];
+        $context = []; // Invalid context without userId
+
+        // Call the trackEvent method
+        $result = $vwoClient->trackEvent($eventName, $context, $eventProperties);
+
+        // Assert that the method resolves with the correct data
+        $this->assertEquals([$eventName => false], $result);
+    }
+}
+
+?>
---
+++ tests/unit/SegmentationEvaluatorTest.php
@@ -0,0 +1,352 @@
+<?php
+
+/**
+ * Copyright 2024 Wingify Software Pvt. Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace vwo;
+
+use PHPUnit\Framework\TestCase;
+use vwo\Packages\SegmentationEvaluator\Evaluators\SegmentEvaluator;
+
+class SegmentationEvaluatorTest extends TestCase
+{
+    protected $testsData;
+    protected $settingsFile;
+
+    protected function setUp(): void
+    {
+        // Initialize data
+        $data = SettingsAndTestCases::get();
+        $this->testsData = $data['SEGMENTATION_TESTS'];
+        $this->settingsFile = $data['BASIC_ROLLOUT_SETTINGS'];
+    }
+
+    public function testAndOperator()
+    {
+        $andOperatorDsl = json_decode(json_encode($this->testsData['and_operator']), false);
+        
+        foreach ($andOperatorDsl as $value) {
+            $dsl = $value->dsl;
+            
+            $expectation = $value->expectation;
+            
+            $customVariables = $value->customVariables;
+            
+            $segmentEvaluator = new SegmentEvaluator();
+
+            $preSegmentationResult = $segmentEvaluator->isSegmentationValid($dsl, $customVariables, $this->settingsFile);
+
+            $this->assertEquals($expectation, $preSegmentationResult);
+            
+        }
+    }
+
+    public function testCaseInsensitiveEqualityOperand()
+    {
+        $caseInsensitiveEqualityOperandDsl = json_decode(json_encode($this->testsData['case_insensitive_equality_operand']), false);
+        
+        foreach ($caseInsensitiveEqualityOperandDsl as $value) {
+            $dsl = $value->dsl;
+            
+            $expectation = $value->expectation;
+            
+            $customVariables = $value->customVariables;
+            
+            $segmentEvaluator = new SegmentEvaluator();
+
+            $preSegmentationResult = $segmentEvaluator->isSegmentationValid($dsl, $customVariables, $this->settingsFile);
+
+            $this->assertEquals($expectation, $preSegmentationResult);
+            
+        }
+    }
+
+    public function testComplexAndOrs()
+    {
+        $complexAndOrsDsl = json_decode(json_encode($this->testsData['complex_and_ors']), false);
+        
+        foreach ($complexAndOrsDsl as $value) {
+            $dsl = $value->dsl;
+            
+            $expectation = $value->expectation;
+            
+            $customVariables = $value->customVariables;
+            
+            $segmentEvaluator = new SegmentEvaluator();
+
+            $preSegmentationResult = $segmentEvaluator->isSegmentationValid($dsl, $customVariables, $this->settingsFile);
+
+            $this->assertEquals($expectation, $preSegmentationResult);
+            
+        }
+    }
+
+    public function testComplexDsl()
+    {
+        $complexDslKeys = ['complex_dsl_1', 'complex_dsl_2', 'complex_dsl_3', 'complex_dsl_4'];
+        
+        foreach ($complexDslKeys as $dslKey) {
+            $complexDsl = json_decode(json_encode($this->testsData[$dslKey]), false);
+            
+            foreach ($complexDsl as $value) {
+                $dsl = $value->dsl;
+
+                $expectation = $value->expectation;
+                
+                $customVariables = $value->customVariables;
+                
+                $segmentEvaluator = new SegmentEvaluator();
+
+                $preSegmentationResult = $segmentEvaluator->isSegmentationValid($dsl, $customVariables, $this->settingsFile);
+
+                $this->assertEquals($expectation, $preSegmentationResult);
+                
+            }
+        }
+    }
+
+    public function testContainsOperand()
+    {
+        $containsOperandDsl = json_decode(json_encode($this->testsData['contains_operand']), false);
+        
+        foreach ($containsOperandDsl as $value) {
+            $dsl = $value->dsl;
+            
+            $expectation = $value->expectation;
+            
+            $customVariables = $value->customVariables;
+            
+            $segmentEvaluator = new SegmentEvaluator();
+
+            $preSegmentationResult = $segmentEvaluator->isSegmentationValid($dsl, $customVariables, $this->settingsFile);
+
+            $this->assertEquals($expectation, $preSegmentationResult);
+            
+        }
+    }
+
+    public function testEndsOperand()
+    {
+        $endsWithOperandDsl = json_decode(json_encode($this->testsData['ends_with_operand']), false);
+        
+        foreach ($endsWithOperandDsl as $value) {
+            $dsl = $value->dsl;
+            
+            $expectation = $value->expectation;
+            
+            $customVariables = $value->customVariables;
+            
+            $segmentEvaluator = new SegmentEvaluator();
+
+            $preSegmentationResult = $segmentEvaluator->isSegmentationValid($dsl, $customVariables, $this->settingsFile);
+
+            $this->assertEquals($expectation, $preSegmentationResult);
+            
+        }
+    }
+
+    public function testEqualityOperand()
+    {
+        $equalityOperandDsl = json_decode(json_encode($this->testsData['equality_operand']), false);
+        
+        foreach ($equalityOperandDsl as $value) {
+            $dsl = $value->dsl;
+            
+            $expectation = $value->expectation;
+            
+            $customVariables = $value->customVariables;
+            
+            $segmentEvaluator = new SegmentEvaluator();
+
+            $preSegmentationResult = $segmentEvaluator->isSegmentationValid($dsl, $customVariables, $this->settingsFile);
+
+            $this->assertEquals($expectation, $preSegmentationResult);
+            
+        }
+    }
+
+    public function testDecimalMismatch()
+    {
+        $decimalMismatchDsl = json_decode(json_encode($this->testsData['new_cases_for_decimal_mismatch']), false);
+        
+        foreach ($decimalMismatchDsl as $value) {
+            $dsl = $value->dsl;
+            
+            $expectation = $value->expectation;
+            
+            $customVariables = $value->customVariables;
+            
+            $segmentEvaluator = new SegmentEvaluator();
+
+            $preSegmentationResult = $segmentEvaluator->isSegmentationValid($dsl, $customVariables, $this->settingsFile);
+
+            $this->assertEquals($expectation, $preSegmentationResult);
+            
+        }
+    }
+
+    public function testNotOperator()
+    {
+        $notOperatorDsl = json_decode(json_encode($this->testsData['not_operator']), false);
+        
+        foreach ($notOperatorDsl as $value) {
+            $dsl = $value->dsl;
+            
+            $expectation = $value->expectation;
+            
+            $customVariables = $value->customVariables;
+            
+            $segmentEvaluator = new SegmentEvaluator();
+
+            $preSegmentationResult = $segmentEvaluator->isSegmentationValid($dsl, $customVariables, $this->settingsFile);
+
+            $this->assertEquals($expectation, $preSegmentationResult);
+            
+        }
+    }
+
+    public function testOrOperator()
+    {
+        $orOperatorDsl = json_decode(json_encode($this->testsData['or_operator']), false);
+        
+        foreach ($orOperatorDsl as $value) {
+            $dsl = $value->dsl;
+            
+            $expectation = $value->expectation;
+            
+            $customVariables = $value->customVariables;
+            
+            $segmentEvaluator = new SegmentEvaluator();
+
+            $preSegmentationResult = $segmentEvaluator->isSegmentationValid($dsl, $customVariables, $this->settingsFile);
+            $this->assertEquals($expectation, $preSegmentationResult);
+            
+        }
+    }
+    
+    public function testRegex()
+    {
+        $regexDsl = json_decode(json_encode($this->testsData['regex']), false);
+        
+        foreach ($regexDsl as $value) {
+            $dsl = $value->dsl;
+            
+            $expectation = $value->expectation;
+            
+            $customVariables = $value->customVariables;
+            
+            $segmentEvaluator = new SegmentEvaluator();
+    
+            $preSegmentationResult = $segmentEvaluator->isSegmentationValid($dsl, $customVariables, $this->settingsFile);
+    
+            $this->assertEquals($expectation, $preSegmentationResult);
+            
+        }
+    }
+    
+    public function testSimpleAndOrs()
+    {
+        $simpleAndOrsDsl = json_decode(json_encode($this->testsData['simple_and_ors']), false);
+        
+        foreach ($simpleAndOrsDsl as $value) {
+            $dsl = $value->dsl;
+            
+            $expectation = $value->expectation;
+            
+            $customVariables = $value->customVariables;
+            
+            $segmentEvaluator = new SegmentEvaluator();
+    
+            $preSegmentationResult = $segmentEvaluator->isSegmentationValid($dsl, $customVariables, $this->settingsFile);
+    
+            $this->assertEquals($expectation, $preSegmentationResult);
+            
+        }
+    }
+    
+    public function testStartsWithOperand()
+    {
+        $startsWithOperandDsl = json_decode(json_encode($this->testsData['starts_with_operand']), false);
+        
+        foreach ($startsWithOperandDsl as $value) {
+            $dsl = $value->dsl;
+            
+            $expectation = $value->expectation;
+            
+            $customVariables = $value->customVariables;
+            
+            $segmentEvaluator = new SegmentEvaluator();
+    
+            $preSegmentationResult = $segmentEvaluator->isSegmentationValid($dsl, $customVariables, $this->settingsFile);
+    
+            $this->assertEquals($expectation, $preSegmentationResult);
+            
+        }
+    }
+    
+    public function testSpecialCharacters()
+    {
+        $specialCharactersDsl = json_decode(json_encode($this->testsData['special_characters']), false);
+        
+        foreach ($specialCharactersDsl as $value) {
+            $dsl = $value->dsl;         
+            $expectation = $value->expectation;          
+            $customVariables = $value->customVariables;          
+            $segmentEvaluator = new SegmentEvaluator();  
+            $preSegmentationResult = $segmentEvaluator->isSegmentationValid($dsl, $customVariables, $this->settingsFile);
+             
+            $this->assertEquals($expectation, $preSegmentationResult);
+            
+        }
+    }
+    
+    public function testUserOperandEvaluator()
+    {
+        $userOperandEvaluatorDsl = json_decode(json_encode($this->testsData['user_operand_evaluator']), false);
+        
+        foreach ($userOperandEvaluatorDsl as $value) {
+            $dsl = $value->dsl;          
+            $expectation = $value->expectation;        
+            $customVariables = $value->customVariables;        
+            $segmentEvaluator = new SegmentEvaluator();  
+
+            $preSegmentationResult = $segmentEvaluator->isSegmentationValid($dsl, $customVariables, $this->settingsFile);
+    
+            $this->assertEquals($expectation, $preSegmentationResult);
+            
+        }
+    }
+    
+    public function testUserOperandEvaluatorWithCustomVariables()
+    {
+        $userOperandEvaluatorWithCustomVariablesDsl = json_decode(json_encode($this->testsData['user_operand_evaluator_with_customVariables']), false);
+        
+        foreach ($userOperandEvaluatorWithCustomVariablesDsl as $value) {
+            $dsl = $value->dsl;
+            
+            $expectation = $value->expectation;
+            
+            $customVariables = $value->customVariables;
+            
+            $segmentEvaluator = new SegmentEvaluator();
+    
+            $preSegmentationResult = $segmentEvaluator->isSegmentationValid($dsl, $customVariables, $this->settingsFile);
+    
+            $this->assertEquals($expectation, $preSegmentationResult);
+            
+        }
+    }   
+}
