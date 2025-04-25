b441b55ecfc6664667ec225cb9e740452dfcf191
Pawan Yadav <pawan.yadav@vwo.com>
2025-04-25 20:47:17 +0530

    fix: base URL issue for EU and Asia

--- src/Constants/Constants.php
+++ src/Constants/Constants.php
@@ -40,7 +40,7 @@ class Constants {
     const DEFAULT_EVENTS_PER_REQUEST = 100;
     const SDK_NAME = 'vwo-fme-php-sdk';
 
-    const SDK_VERSION = '1.7.0';
+    const SDK_VERSION = '1.7.1';
     const AP = 'server';
 
     const SETTINGS = 'settings';
